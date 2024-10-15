95abb2606c296c544e2095a64be5ed12895d8dc3
Viery Nugroho <viery15102002@gmail.com>
2024-10-15 10:59:28 +0700

    fix: route http method on withdrawal, deposit, transfer, log

--- src/routes/bankingSystem.js
+++ src/routes/bankingSystem.js
@@ -3,9 +3,9 @@ import { BankingSystemController } from '../controllers/bankingSystem.js';
 const router = express.Router();
 
 router.route('/balance/:id').get(BankingSystemController.getBalance);
-router.route('/deposit').put(BankingSystemController.deposit);
-router.route('/withdrawal').put(BankingSystemController.withdrawal);
-router.route('/transfer').put(BankingSystemController.transfer);
-router.route('/log').get(BankingSystemController.log);
+router.route('/deposit/:id').put(BankingSystemController.deposit);
+router.route('/withdrawal/:id').put(BankingSystemController.withdrawal);
+router.route('/transfer/:id').put(BankingSystemController.transfer);
+router.route('/log/:id').get(BankingSystemController.log);
 
 export default router;
